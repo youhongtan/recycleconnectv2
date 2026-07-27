623c245a06180fd29f8942b771cd6fd1f0a47537
Y Hong <youhong.tyh@gmail.com>
2026-07-27 18:31:03 +0800

    fix: convert to commonjs for vercel compat

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
-import path from 'path'
+const react = require('@vitejs/plugin-react')
+const { defineConfig, loadEnv } = require('vite')
+const path = require('path')
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+module.exports = defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   function requireKey(name) {
